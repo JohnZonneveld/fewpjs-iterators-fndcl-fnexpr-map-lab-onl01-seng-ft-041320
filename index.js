adba418585977a9fb8024de93f2b9f0139f0d4ee
John Zonneveld <jjczon@gmail.com>
2020-07-30 21:57:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,12 @@ const tutorials = [
 
 const titleCased = () => {
   return tutorials
+  return tutorials.map(function titleCase(str) {
+    var splitStr = str.split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    return splitStr.join(' '); 
+ })
+}
 }
